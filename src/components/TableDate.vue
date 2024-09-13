93279c856b17df6d7a77da2a5380a12284e5d643
Jeisonlopez259911 <jeisonlopez2599@gmail.com>
2024-09-12 20:15:03 -0500

    Tabla vue 3

--- src/components/TableDate.vue
+++ src/components/TableDate.vue
@@ -1,5 +1,6 @@
 <script setup>
 import SelectedNumber from './SelectedNumber.vue';
+import Pagination from './Pagination.vue';
 </script>
 
 <template>
@@ -67,42 +68,10 @@ import SelectedNumber from './SelectedNumber.vue';
         </div>
       </div>
     </div>
-    <div class="columns-2">
-      <div class="flex justify-end whitespace-nowrap">
+      <div class="flex flex-row justify-end whitespace-nowrap">
         <SelectedNumber />
+        <Pagination/>
       </div>
-
-      <div class="flex justify-end whitespace-nowrap">
-        <nav aria-label="Page navigation example">
-          <ul class="list-style-none flex">
-            <li>
-              <a
-                class="pointer-events-none relative block rounded-full bg-transparent px-3 py-1.5 text-sm text-surface/50 transition duration-300 dark:text-neutral-400"><v-icon
-                  name="fa-angle-left" /></a>
-            </li>
-            <li>
-              <a class="relative block rounded-full bg-transparent px-3 py-1.5 text-sm text-surface transition duration-300 hover:bg-neutral-100 focus:bg-neutral-100 focus:text-primary-700 focus:outline-none active:bg-neutral-100 active:text-primary-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700  dark:focus:text-primary-500 dark:active:bg-neutral-700 dark:active:text-primary-500"
-                href="#!">1</a>
-            </li>
-            <li aria-current="page">
-              <a class="relative block rounded-full bg-primary-100 px-3 py-1.5 text-sm font-medium text-primary-700 transition duration-300 focus:outline-none dark:bg-slate-900 dark:text-primary-500"
-                href="#!">2
-                <span
-                  class="absolute -m-px h-px w-px overflow-hidden whitespace-nowrap border-0 p-0 [clip:rect(0,0,0,0)]">(current)</span>
-              </a>
-            </li>
-            <li>
-              <a class="relative block rounded-full bg-transparent px-3 py-1.5 text-sm text-surface transition duration-300 hover:bg-neutral-100 focus:bg-neutral-100 focus:text-primary-700 focus:outline-none active:bg-neutral-100 active:text-primary-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:focus:text-primary-500 dark:active:bg-neutral-700 dark:active:text-primary-500"
-                href="#!">3</a>
-            </li>
-            <li>
-              <a class="relative block rounded-full bg-transparent px-3 py-1.5 text-sm text-surface transition duration-300 hover:bg-neutral-100 focus:bg-neutral-100 focus:text-primary-700 focus:outline-none active:bg-neutral-100 active:text-primary-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:focus:text-primary-500 dark:active:bg-neutral-700 dark:active:text-primary-500"
-                href="#!"><v-icon name="fa-angle-right" /></a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
 
   </div>
 </template>
